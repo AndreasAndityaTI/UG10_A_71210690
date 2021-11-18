05d26d0ecfac07b80b142f3d092fcb628fe6b28b
AndreasAndityaTI <93309168+AndreasAndityaTI@users.noreply.github.com>
2021-11-18 18:49:35 +0700

    1_A_71210690

---
+++ 1_A_71210690.py
@@ -0,0 +1,29 @@
+ug=int(input("Masukkan nilai rata-rata UG anda: "))
+tts=int(input("Masukkan nilai TTS anda: "))
+tas=int(input("Masukkan nilai TAS anda: "))
+rug=ug*(70/100)
+rtts=tts*(15/100)
+rtas=tas*(15/100)
+total=rtts+rtas+rug
+print("=========================")
+print("Nilai anda: ",total)
+if(total>=85):
+    grade="A"
+elif(total>=80):
+    grade="A-"
+elif(total>=75):
+    grade="B+"
+elif(total>=70):
+    grade="B"
+elif(total>=65):
+    grade="B-"
+elif(total>=60):
+    grade="C+"
+elif(total>=55):
+    grade="C"
+elif(total>=45):
+    grade="D"
+else:
+    grade="E"
+print("Nilai huruf anda: ",grade)
+
